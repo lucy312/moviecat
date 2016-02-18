03e942bec726da757d07de60e9cd5898e8d7051c
csm <csm@qq.com>
2016-02-18 21:23:41 +0800

    左侧导航实现焦点绑定

--- app/app.js
+++ app/app.js
@@ -10,5 +10,17 @@
     ]).
     config(['$routeProvider', function($routeProvider) {
         $routeProvider.otherwise({ redirectTo: '/in_theaters/1' });
+    }]).
+    controller('AppController',['$scope','$location',function($scope,$location){
+    	$scope.location=$location;
+     $scope.$watch('location.path()',function(now){
+       if(now.startsWith('/coming_soon')){
+       	$scope.type='coming_soon';
+       }else if(now.startsWith('/top250')){
+       	$scope.type='top250';
+       }else if(now.startsWith('/in_theaters')){
+       	$scope.type='in_theaters';
+       }
+     });
     }]);
 })(angular);
