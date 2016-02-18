73545aa46f4e184705b0d19939a892a28fd9e53b
csm <csm@qq.com>
2016-02-18 16:20:38 +0800

    数据绑定补充

--- app/coming_soon/view.js
+++ app/coming_soon/view.js
@@ -1,16 +1,23 @@
 (function(angular) {
     'use strict';
-
-    var module = angular.module('myApp.coming_soon', ['ngRoute'])
-
+    // 创建正在热映模块
+    var module = angular.module('myApp.coming_soon', ['ngRoute','myApp.service.jsonp'])
+        // 配置模块路由
     module.config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/coming_soon', {
             templateUrl: 'coming_soon/view.html',
             controller: 'ComingSoonController'
         });
     }]);
-
-    module.controller('ComingSoonController', ['$scope', function($scope) {
-    	console.log(2)
+    //配置模块控制器
+    module.controller('ComingSoonController', ['$scope','HttpService', function($scope,HttpService) {
+    	$scope.title='';
+    	$scope.message='';
+    	$scope.subjects=[];
+    	HttpService.jsonp('http://api.douban.com/v2/movie/coming_soon',{},function(data){
+          $scope.subjects=data.subjects;
+          $scope.title=data.title;
+          $scope.$apply();//重新同步绑定数据
+    	})
     }]);
 })(angular);
--- app/top250/view.js
+++ app/top250/view.js
@@ -1,16 +1,23 @@
 (function(angular) {
     'use strict';
-
-    var module = angular.module('myApp.top250', ['ngRoute'])
-
+    // 创建正在热映模块
+    var module = angular.module('myApp.top250', ['ngRoute','myApp.service.jsonp'])
+        // 配置模块路由
     module.config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/top250', {
             templateUrl: 'top250/view.html',
             controller: 'Top250Controller'
         });
     }]);
-
-    module.controller('Top250Controller', ['$scope', function($scope) {
- 		console.log(3)
+    //配置模块控制器
+    module.controller('Top250Controller', ['$scope','HttpService', function($scope,HttpService) {
+    	$scope.title='';
+    	$scope.message='';
+    	$scope.subjects=[];
+    	HttpService.jsonp('http://api.douban.com/v2/movie/top250',{},function(data){
+          $scope.subjects=data.subjects;
+          $scope.title=data.title;
+          $scope.$apply();//重新同步绑定数据
+    	})
     }]);
 })(angular);
