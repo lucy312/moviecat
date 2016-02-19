55bdd16b4138ff5a7d56a50889f23cd7265c162a
csm <csm@qq.com>
2016-02-19 23:22:29 +0800

    用angular自定义属性指令的方法实现左侧导航焦点绑定

--- app/app.js
+++ app/app.js
@@ -5,20 +5,22 @@
     angular.module('myApp', [
         'ngRoute',
         'myApp.moviecat',
+        'myApp.directives'
     ]).
     config(['$routeProvider', function($routeProvider) {
         $routeProvider.otherwise({ redirectTo: '/in_theaters/1' });
-    }]).
-    controller('AppController',['$scope','$location',function($scope,$location){
-    	$scope.location=$location;
-     $scope.$watch('location.path()',function(now){
-       if(now.startsWith('/coming_soon')){
-       	$scope.type='coming_soon';
-       }else if(now.startsWith('/top250')){
-       	$scope.type='top250';
-       }else if(now.startsWith('/in_theaters')){
-       	$scope.type='in_theaters';
-       }
-     });
-    }]);
+    }])
+    // 用自定义angular属性指令的方法去实现以下功能，所以此段代码注释
+    // .controller('AppController',['$scope','$location',function($scope,$location){
+    // 	$scope.location=$location;
+    //  $scope.$watch('location.path()',function(now){
+    //    if(now.startsWith('/coming_soon')){
+    //    	$scope.type='coming_soon';
+    //    }else if(now.startsWith('/top250')){
+    //    	$scope.type='top250';
+    //    }else if(now.startsWith('/in_theaters')){
+    //    	$scope.type='in_theaters';
+    //    }
+    //  });
+    // }]);
 })(angular);
---
+++ app/components/auto_focus.js
@@ -0,0 +1,31 @@
+/*
+ * @Author: Administrator
+ * @Date:   2016-02-19 22:04:26
+ * @Last Modified by:   Administrator
+ * @Last Modified time: 2016-02-19 23:18:59
+ */
+
+(function(angular) {
+    'use strict';
+    angular.module('myApp.directives', [])
+        .directive('autoFocus', ['$location', function($location) {
+            return {
+                restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+                link: function($scope, iElm, iAttrs, controller) {
+
+                    $scope.$location = $location;
+                    $scope.$watch('$location.path()', function(now) {
+                        // 当path发生变化时执行，now是变化后的值
+                        var aLink = iElm.children().attr('href');
+                        var type = aLink.replace(/#(\/.+?)\/\d+/, '$1'); // /coming_soon
+                        if (now.startsWith(type)) {
+                            // 访问的是当前链接
+                            iElm.parent().children().removeClass('active');
+                            iElm.addClass('active');
+                            window.ele=iElm;
+                        }
+                    });
+                }
+            };
+        }]);
+})(angular);
